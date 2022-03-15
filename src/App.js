6e52fa5bfbf4a77e2129bf19c16f82e411f9b7b7
bhargesh44 <bhargeshgediya44@gmail.com>
2022-03-15 10:55:33 +0530

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>My React App</h1>
-      <h2>Author: Bhargesh Gediya</h2>
-      <img src={logo} alt="" />
+      <h2>---- Bhargesh Gediya ----</h2>
     </div>
   );
 }
